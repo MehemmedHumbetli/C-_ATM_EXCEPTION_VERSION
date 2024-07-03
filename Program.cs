ffa81926bd881d43235e243b246cc15c979e6a04
MehemmedLinux <mehemedhumbetov1@example.com>
2024-07-03 19:53:39 +0400

    first commit

--- Program.cs
+++ Program.cs
@@ -1,82 +1,34 @@
-﻿class program
+﻿class Program
 {
-
-    abstract class Shape
+    class Dictionary
     {
-        public abstract void perimeter();
-        public abstract void area();
-        public override string ToString()
+        public Dictionary<string, string> translate = new Dictionary<string, string>();
+
+        public string this[string word]
         {
-            return "This is a shape";
+            get
+            {
+                try
+                {
+                    return translate[word];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return "Word could not found";
+                }
+            }
+            set
+            {
+                translate[word] = value;
+            }
         }
     }
-
-
-    class Rectangle : Shape
-    {
-        public double En { get; set; }
-        public double Uzunluq { get; set; }
-
-        public override void area()
-        {
-            Console.WriteLine($"Area = {En * Uzunluq}");
-        }
-        public override void perimeter()
-        {
-            Console.WriteLine($"Perimeter = {2 * (En + Uzunluq)}");
-        }
-
-        public override string ToString()
-        {
-            return $"Eni: {En}   Uzunluq: {Uzunluq}";
-        }
-    }
-
-    class Square : Rectangle
-    {
-        public double teref { get; set; }
-
-        public override void area()
-        {
-            Console.WriteLine($"Area = {teref} * {teref}");
-        }
-
-        public override void perimeter()
-        {
-            Console.WriteLine($"Perimeter = {4 * teref}");
-        }
-
-        public override string ToString()
-        {
-            return $"Teref: {teref}";
-        }
-    }
-
-    class Circle : Shape
-    {
-        public double Radius { get; set; }
-
-        public override void perimeter()
-        {
-            Console.WriteLine($"Perimeter = {2 * 3.14 * Radius}");
-        }
-
-        public override void area()
-        {
-            Console.WriteLine($"Area = {3.14 * Radius * Radius}");
-        }
-
-        public override string ToString()
-        {
-            return $"Radius: {Radius}";
-        }
-    }
-
-
-
-
     static void Main(string[] args)
     {
+        Dictionary dictonary = new Dictionary();
 
+        dictonary["masin"] = "car";
+
+        Console.WriteLine($"masin : {dictonary["masin"]}");
     }
 }
